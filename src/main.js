c17f30f328f52b95d5d299a933bf885288f04359
wangkun1-jk <wangkun1-jk@360shuke.com>
2024-08-14 21:06:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,50 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.prototype.$axios = axios
+Vue.use(VueAxios, axios)
+axios.defaults.headers.common['Authorization'] = ''
 
+require('./mock')
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import qs from 'qs';
+Vue.prototype.$qs = qs;
+
+import store from './store/index';
+Vue.prototype.$store = store;
+
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  render: h => h(App)
-}).$mount('#app')
+  store,
+  components: { App },
+  template: '<App/>'
+})
+
+axios.interceptors.request.use(
+  config => {
+    console.log(config);
+    if (config.url != '/api/login') {
+      if (sessionStorage.getItem('Authorization')) {
+        config.headers.Authorization = sessionStorage.getItem('Authorization')
+      } else {
+        console.log('to login')
+        this.$router.push("/");
+      }
+    }
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+)
--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,21 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import HelloWorld from '../components/HelloWorld.vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/login',
-    component: () => import('@/views/login/index')
-  },
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/hello',
-    name: 'hello',
-    component: HelloWorld
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    }
+  ]
 })
-
-export default router
